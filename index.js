73d36069f9d95bbe66520394396d65247de98cbb
Yudi Edri Alviska <yudi.a@weefer.co.id>
2019-01-28 09:00:38 +0700

    remove https

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import https from 'https';
-
 module.exports = function tiny(string) {
     if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
     return string.replace(/\s/g, "");
